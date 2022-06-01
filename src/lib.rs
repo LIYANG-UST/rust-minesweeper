6e94df4f3ab8e29861319ff336c5b395ac06040c
liyang-win11 <ylikp@connect.ust.hk>
2022-06-02 00:41:25 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+mod random;
+
+use std::collections::HashSet;
+
+use random::random_range;
+
+pub type Position = (usize, usize);
+
+pub enum OpenResult {
+    Mine,
+    NoMine(u8),
+}
+
+#[derive(Debug)]
+pub struct Minesweeper {
+    width: usize,
+    height: usize,
+    open_fields: HashSet<Position>,
+    mines: HashSet<Position>,
+    flagged_fields: HashSet<Position>,
+}
+
+impl Minesweeper {
+    pub fn new(width: usize, height: usize, mine_count: usize) -> Minesweeper {
+        Minesweeper {
+            width,
+            height,
+            open_fields: HashSet::new(),
+            mines: {
+                let mut mines = HashSet::new();
+
+                while mines.len() < mine_count {
+                    mines.insert((random_range(0, width), random_range(0, height)));
+                }
+                mines
+            },
+            flagged_fields: HashSet::new(),
+        }
+    }
+
+    pub fn neighbors(&self, (x, y): Position) -> Vec<Position> {
+        (x - 1..=x + 1)
+            .flat_map(|i| (y - 1..=y + 1).map(move |j| (i, j)))
+            .collect()
+    }
+
+    pub fn open(&mut self, position: Position) -> OpenResult {
+        self.open_fields.insert(position);
+
+        let is_mine = self.mines.contains(&position);
+
+        if is_mine {
+            OpenResult::Mine
+        } else {
+            OpenResult::NoMine(0)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Minesweeper;
+
+    #[test]
+    fn test() {
+        let ms = Minesweeper::new(10, 10, 5);
+
+        println!("{:?}", ms);
+    }
+}
---
+++ src/random.rs
@@ -0,0 +1,8 @@
+use rand::{thread_rng, Rng};
+
+pub fn random_range(min: usize, max: usize) -> usize {
+    let mut rng = thread_rng();
+
+    rng.gen_range(min..max)
+}
+
